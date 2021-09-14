326a134a8892b937248d1f726cf8230c9afb75f2
Lucas-Braz7x <lucasdutrabraz@gmail.com>
2021-09-14 19:29:37 -0300

    Estiliza o componente Skeleton

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,8 +8,7 @@ export const Wrapper = styled.div`
 
 export const ContainerSearch = styled.aside`
   background: ${(props) => props.theme.colors.background};
-  max-width: 360px;
-  width: 100%;
+  width: 360px;
   height: 100vh;
   overflow-y: auto;
   overflow-x:hidden;
@@ -35,7 +34,7 @@ export const ContainerMap = styled.div`
 
 export const Carousel = styled(Slider)`
   .slick-slide{
-    margin-right: 8px;
+    margin-right: 20px;
   }
 `;
 
@@ -45,4 +44,31 @@ export const CarouselTitle = styled.h1`
   font-size: 24px;
   font-weight: 700;
   line-height: 29px;
+`;
+
+export const ModalTitle = styled.p`
+  margin-bottom: 10px;
+
+  letter-spacing: 0.11px;
+  line-height: 29px;
+  font-size: 24px;
+  font-weight: 700;
+
+  text-transform: none;
+  color: ${(props) => props.theme.colors.text};
+  font-family: ${(props) => props.theme.fonts.regular};
+`;
+
+export const ModalContent = styled.p`
+  ${ModalTitle}; /* Pegando('estendendo') as propriedades do ModalTitle */
+
+  text-transform: none;
+  color: ${(props) => props.theme.colors.text};
+  font-family: ${(props) => props.theme.fonts.regular};
+  margin-bottom: 10px;
+  letter-spacing: 0.11px;
+
+  line-height: 19px;
+  font-size: 16px;
+  font-weight: 400;
 `;
